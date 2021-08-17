294bf13b04c369a508359c2369fb2e0405eeb2f3
zhaoxiaowei <zhaoxiaowei@efy-tech.com>
2021-08-17 11:15:29 +0800

    add efy meta to bb layer

--- imx/tools/fsl-setup-release.sh
+++ imx/tools/fsl-setup-release.sh
@@ -195,6 +195,7 @@ echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-python \"" >> $BU
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-filesystems \"" >> $BUILD_DIR/conf/bblayers.conf
 
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-qt5 \"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \" \${BSPDIR}/sources/meta-efy-imx6ull \"" >> $BUILD_DIR/conf/bblayers.conf
 
 echo BSPDIR=$BSPDIR
 echo BUILD_DIR=$BUILD_DIR
